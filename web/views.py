111e1cdcf948de40e69de0c089385702a12f9297
Kyoseung Koo <koo@dbs.snu.ac.kr>
2024-03-15 09:07:12 +0000

    add state machine for reading log; dp draft

--- web/views.py
+++ web/views.py
@@ -1,6 +1,7 @@
 import psycopg2
 import os
 import time
+import re
 
 from django.shortcuts import render
 
@@ -20,10 +21,290 @@ def read_and_clear_log():
 
     ret = None
     with open(filename, 'r') as f:
-        ret = '\n'.join(f.readlines())
+        ret = f.readlines()
 
     return ret
 
+
+def parse_path_with_state_machine(logs: list, cur: int):
+    """
+    state list:
+    PathHeader, PathKeys, PathJoin, PathMJoin, PathOuter, PathInner
+    PathWait, PathWait2
+    PathDone
+    """
+
+    state = 'PathHeader'
+    path_buffer = {}
+
+    while state != 'PathDone':
+        line = logs[cur].strip()
+        print(state, line)
+        #input()
+
+        if state == 'PathHeader':
+            _PATHHEADER_EXP = r'\ *(\w*)\((.*)\) required_outer \((\w*)\) rows=(\d*) cost=(\d*\.\d*)\.\.(\d*\.\d*)'
+            _PATHHEADER_EXP_NOPARAM = r'\ *(\w*)\((.*)\) rows=(\d*) cost=(\d*\.\d*)\.\.(\d*\.\d*)'
+
+            # get the header that is must be in the logs
+            header = re.match(_PATHHEADER_EXP, line)
+            node, relid, ro_relid, rows, startup_cost, total_cost = None, None, None, None, None, None
+            if header:
+                node, relid, ro_relid, rows, startup_cost, total_cost = header.groups()
+            else:
+                header = re.match(_PATHHEADER_EXP_NOPARAM, line)
+                assert(header)
+                node, relid, rows, startup_cost, total_cost = header.groups()
+
+            path_buffer['node'] = node
+            path_buffer['relid'] = relid
+            path_buffer['ro_relid'] = ro_relid
+            path_buffer['rows'] = int(rows)
+            path_buffer['startup_cost'] = float(startup_cost)
+            path_buffer['total_cost'] = float(total_cost)
+
+
+            state = 'PathWait'
+            cur += 1
+
+        elif state == 'PathWait':
+            # a temp state to decide if it is PathKeys, PathJoin, or PathMJoin
+            _PATHKEYS_EXP = r'\ *pathkeys:\ (.*)'
+            _CLAUSES_EXP = r'\ *clauses:(.*)'
+            _MERGEJOIN_INFO_EXP = r'\ *sortouter=(\d) sortinner=(\d) materializeinner=(\d)'
+
+            if re.match(_PATHKEYS_EXP, line):
+                state = 'PathKeys'
+            elif re.match(_CLAUSES_EXP, line):
+                state = 'PathJoin'
+            elif re.match(_MERGEJOIN_INFO_EXP, line):
+                state = 'PathMJoin'
+            else:
+                state = 'PathDone'
+
+        elif state == 'PathKeys':
+            _PATHKEYS_EXP = r'\ *pathkeys:\ (.*)'
+            pathkeys = re.match(_PATHKEYS_EXP, line)
+            assert(pathkeys)
+            path_buffer['pathkeys'] = pathkeys.groups()[0].strip()
+
+            state = 'PathWait'
+            cur += 1
+
+        elif state == 'PathJoin':
+            _CLAUSES_EXP = r'\ *clauses:(.*)'
+            clauses = re.match(_CLAUSES_EXP, line)
+            assert(clauses)
+
+            path_buffer['join'] = {
+                'clauses': clauses.groups()[0].strip()
+            }
+
+            state = 'PathWait2'
+            cur += 1
+
+        elif state == 'PathMJoin':
+            _MERGEJOIN_INFO_EXP = r'\ *sortouter=(\d) sortinner=(\d) materializeinner=(\d)'
+            mj_info = re.match(_MERGEJOIN_INFO_EXP, line)
+            assert(mj_info)
+
+            outerkeys_exist, innerkeys_exist, m_inner_exist = mj_info.groups()
+            path_buffer['join']['mergejoin_info'] = {
+                'outerkeys_exist': outerkeys_exist,
+                'innerkeys_exist': innerkeys_exist,
+                'm_inner_exist': m_inner_exist
+            }
+
+            state = 'PathOuter'
+            cur += 1
+
+        elif state == 'PathWait2':
+            # a temp state to decide the new line is for MJoin or outer path
+            _MERGEJOIN_INFO_EXP = r'\ *sortouter=(\d) sortinner=(\d) materializeinner=(\d)'
+
+            if re.match(_MERGEJOIN_INFO_EXP, line):
+                state = 'PathMJoin'
+            else:
+                state = 'PathOuter'
+
+        elif state == 'PathOuter':
+            outer, _cur = parse_path_with_state_machine(logs, cur)
+            path_buffer['join']['outer'] = outer
+
+            state = 'PathInner'
+
+        elif state == 'PathInner':
+            inner, _cur = parse_path_with_state_machine(logs, cur)
+            path_buffer['join']['inner'] = inner 
+
+            state = 'PathDone'
+
+    return path_buffer, cur
+
+    
+
+def parse_with_state_machine(logs: list, cur: int, _START_SIGN: str, _END_SIGN: str):
+    """
+    state list:
+        Start
+        RelOptHeader, RelOptPathlist
+        Path (PathHeader, PathKeys, PathJoin, PathMJoin)
+        Done
+    """
+    state = 'Start'
+    buffer = {}
+
+    while state != 'Done':
+        line = logs[cur].strip()
+        print(state, line, cur)
+
+        if state == 'Start':
+            if _START_SIGN in line:
+                state = 'RelOptHeader'
+
+            cur += 1
+
+        elif state == 'RelOptHeader':
+            _RELINFO_EXP = r'RELOPTINFO \((.*)\): rows=(\d*) width=(\d*)'
+
+            # get relinfo that is must be in the logs
+            relinfo = re.match(_RELINFO_EXP, line)
+            assert(relinfo)
+
+            relid, rows, width = relinfo.groups()
+            buffer = {
+                'relid': relid,
+                'rows': int(rows),
+                'width': int(width)
+            }
+
+            state = 'Wait'
+            cur += 1
+
+        elif state == 'Wait':
+            _PATH_LIST_EXP = 'path list:'
+            _CHEAPESTPARAMPATH_LIST_EXP = 'cheapest parameterized paths:'
+            _CHEAPESTSTARTUPPATH_EXP = 'cheapest startup path:'
+            _CHEAPESTTOTALPATH_EXP = 'cheapest total path:'
+
+            if _PATH_LIST_EXP in line:
+                state = 'PathList'
+            elif _CHEAPESTPARAMPATH_LIST_EXP in line:
+                state = 'CheapestParamPathList'
+            elif _CHEAPESTSTARTUPPATH_EXP in line:
+                state = 'CheapestStartupPath'
+                cur += 1
+            elif _CHEAPESTTOTALPATH_EXP in line:
+                state = 'CheapestTotalPath'
+                cur += 1
+            elif _END_SIGN in line:
+                state = 'Done'
+                cur += 1
+            else:
+                cur += 1
+
+        elif state == 'PathList':
+            buffer['paths'] = []
+
+            state = 'Path'
+            cur += 1
+
+        elif state == 'Path':
+            _path_buffer, _cur = parse_path_with_state_machine(logs, cur)
+            buffer['paths'].append(_path_buffer)
+
+            state = 'PathContinue'
+            cur = _cur
+
+        elif state == 'PathContinue':
+            strip = line.replace('\t', '').replace('\n', '').strip()
+            if strip != '':
+                state = 'Path'
+            else:
+                state = 'Wait'
+                cur += 1
+
+        elif state == 'CheapestParamPathList':
+            buffer['cheapest_param_paths'] = []
+
+            state = 'CheapestParamPath'
+            cur += 1
+
+        elif state == 'CheapestParamPath':
+            _path_buffer, _cur = parse_path_with_state_machine(logs, cur)
+            buffer['cheapest_param_paths'].append(_path_buffer)
+
+            state = 'CheapestParamPathContinue'
+            cur = _cur
+
+        elif state == 'CheapestParamPathContinue':
+            strip = line.replace('\t', '').replace('\n', '').strip()
+            if strip != '':
+                state = 'CheapestParamPath'
+            else:
+                state = 'Wait'
+                cur += 1
+
+        elif state == 'CheapestStartupPath':
+            _path_buffer, _cur = parse_path_with_state_machine(logs, cur)
+            buffer['cheapest_startup_paths'] = _path_buffer
+
+            state = 'Wait'
+            cur = _cur
+
+        elif state == 'CheapestTotalPath':
+            _path_buffer, _cur = parse_path_with_state_machine(logs, cur)
+            buffer['cheapest_total_paths'] = _path_buffer
+
+            state = 'Wait'
+            cur = _cur
+
+    return buffer, cur
+
+
+def get_base_path(log_lines: list, cur: int):
+    _START_SIGN = '[VPQO][BASE] set_rel_pathlist started'
+    _END_SIGN = '[VPQO][BASE] set_rel_pathlist done'
+    return parse_with_state_machine(log_lines, cur, _START_SIGN, _END_SIGN)
+
+
+def get_dp_path(log_lines: list, cur: int):
+    _START_SIGN = '[VPQO][DP] standard_join_search started'
+    _END_SIGN = '[VPQO][DP] standard_join_search done'
+    return parse_with_state_machine(log_lines, cur, _START_SIGN, _END_SIGN)
+
+
+def process_log(log_lines):
+    ret = {
+        'type': 'dp',
+        'base': [],
+        'geqo': {},
+        'dp': [] 
+    }
+
+    if '[GEQO]' in ''.join(log_lines):
+        ret['type'] = 'geqo'
+
+    _START_BASE_SIGN = '[VPQO][BASE] set_rel_pathlist started'
+    _START_DP_SIGN = '[VPQO][DP] standard_join_search started'
+
+    cur = 0
+    while cur < len(log_lines):
+        line = log_lines[cur].strip()
+        if _START_BASE_SIGN in line:
+            base, _cur = get_base_path(log_lines, cur)
+            ret['base'].append(base)
+            cur = _cur - 1
+
+        if _START_BASE_SIGN in line:
+            dp, _cur = get_dp_path(log_lines, cur)
+            ret['dp'].append(dp)
+            cur = _cur - 1
+
+        cur += 1
+
+    return ret
+        
 class QueryView(APIView):
     def post(self, request, format=None):
         # SQL 공격이 근본적으로 가능하므로, 절대 링크를 외부공개 하지 마세요.
@@ -38,7 +319,8 @@ class QueryView(APIView):
         cur.execute(q)        # Execute a query
         records = cur.fetchall()     # Retrieve query results
 
-        log = read_and_clear_log()
+        log_lines = read_and_clear_log()
+        opt_data = process_log(log_lines)
 
         # return
-        return Response({'result': str(records), 'log': log})
+        return Response({'query': q, 'result': str(records), 'optimizer': opt_data})
