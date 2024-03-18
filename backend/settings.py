fb418befb1d73743847b134ede73ee612ba45ac0
ratdbs <juheehan@dbs.snu.ac.kr>
2024-03-18 13:11:24 +0900

    settings & view 적용

--- backend/settings.py
+++ backend/settings.py
@@ -86,9 +86,13 @@ CORS_ALLOWED_ORIGINS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+ 'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'queryplans',
+        'USER': 'postgres',
+        'PASSWORD': 'dbs402418',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 
--- web/views.py
+++ web/views.py
@@ -11,7 +11,7 @@ class QueryView(APIView):
         q = request.data.get('query', 'EXPLAIN SELECT \'Hello World\'')
         
         # get query results
-        conn = psycopg2.connect("host=localhost dbname=postgres user=postgres password=mysecretpassword")    # Connect to your postgres DB
+        conn = psycopg2.connect("host=localhost dbname=queryplans user=postgres password=dbs402418")    # Connect to your postgres DB
         cur = conn.cursor()     # Open a cursor to perform database operations
         cur.execute(q)        # Execute a query
         records = cur.fetchall()     # Retrieve query results
